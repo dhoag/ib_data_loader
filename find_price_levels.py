fa75f18cdff92712b774550f0568d0e3432ee503
dhoag <davehoag@gmail.com>
2020-09-27 21:41:42 -0500

    Print price levels

--- find_price_levels.py
+++ find_price_levels.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib.dates import DateFormatter, SecondLocator, date2num
 import pandas as pd
 
+
 def main(args):
     es_bars = None
     for aFile in args.files: 
@@ -20,8 +21,18 @@ def main(args):
     es_bars[['Open','High', 'Low', 'Close', 'Volume', 'count', 'WAP']] = es_bars[['Open','High', 'Low', 'Close', 'Volume', 'count', 'WAP']].apply(pd.to_numeric)
     es_bars['date_time'] = es_bars.date.apply(str) + ' ' +es_bars.timestamp
     es_bars.date_time = es_bars.date_time.apply(pd.to_datetime)
-    plt.bar(es_bars['WAP'], es_bars['Volume'])
-    plt.show()
+    es_bars['wap_rnd'] = es_bars['WAP'].round(0)
+    grouped_bars : pd.DataFrame = es_bars.groupby('wap_rnd').sum()
+    grouped_bars = grouped_bars[['Volume']]
+    counts : pd.Series = es_bars.groupby('wap_rnd').count()
+    grouped_bars['bar_count'] = counts['Close']
+    grouped_bars.to_csv('tmp.csv', header=True)
+    #print(grouped_bars.describe())
+    #print(counts.describe())
+
+    #plt.bar(es_bars['WAP'], es_bars['Volume'])0:w
+
+    #plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analyze volume at WAP price levels')
